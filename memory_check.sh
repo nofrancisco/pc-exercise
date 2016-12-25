3f6820f7c4e191ef46feff5d1f67c3611fe4466f
Nico <nicoloofrancisco@gmail.com>
2016-12-26 04:12:19 +0800

    positional functions practice

--- memory_check.sh
+++ memory_check.sh
@@ -2,6 +2,10 @@
 
 clear
 
-free
-Total_memory=$(free | grep Mem: | awk '{print $2}')
-echo $Total_memory
+free -m
+Total_memory=$(free -m | grep Mem: | awk '{print $2}')
+echo "Total Memory: " $Total_memory
+Used_memory=$(free -m | grep Mem: | awk '{print $3}')
+echo "Used_memory: " $Used_memory
+Free_memory=$(free -m | grep Mem: | awk '{print $4}')
+echo "Free Memory: " $Free_memory
