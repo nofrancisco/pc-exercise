7cf2f3a5a7a3cce0bc2e3d9fe70f5d70a1e1c874
Nico <nicoloofrancisco@gmail.com>
2016-12-27 14:53:13 +0800

    Added critical and warning values

--- memory_check.sh
+++ memory_check.sh
@@ -1,11 +1,47 @@
-#!/bin/bash
-
+\#!/bin/bash
 clear
-
-free -m
+free
 Total_memory=$(free -m | grep Mem: | awk '{print $2}')
-echo "Total Memory: " $Total_memory
+echo "Total Memory: "$Total_memory "mb"
 Used_memory=$(free -m | grep Mem: | awk '{print $3}')
-echo "Used_memory: " $Used_memory
+echo "Used_memory: "$Used_memory "mb"
 Free_memory=$(free -m | grep Mem: | awk '{print $4}')
-echo "Free Memory: " $Free_memory
+echo "Free Memory: "$Free_memory "mb"
+
+while getopts ":c: :w: :e:" opt; do
+	case $opt in
+		c) 
+				
+			critical_threshold=${OPTARG}
+			echo "Critical Threshold (%) =" $critical_threshold
+			critical_value=$(bc <<< "scale=2; $Total_memory - ($Total_memory * ($critical_threshold/100))")
+			echo $critical_value
+			
+			;;
+			
+		w)
+			warning_threshold=${OPTARG}
+			echo "Warning Threshold (%) =" $warning_threshold
+			warning_value=$(bc <<< "scale=2; $Total_memory - ($Total_memory * ($warning_threshold/100))")
+			echo $warning_value
+			;;
+
+		e)
+			email_address=${OPTARG}
+			echo "Entered email address: " $email_address
+			;;
+
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			;;
+
+		:)
+			echo "Supply Required Arguments" >&2
+			exit 1
+			;;
+	esac
+done
+
+
+		 
+		
--- test.sh
+++ test.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 [-s <45|90>] [-p <string>]" 1>&2; exit 1; }
-
-while getopts ":s:p:" o; do
-	case "${o}" in
-		s)
-			s=${OPTARG}
-			((s == 45 || s == 90)) || usage
+while getopts ":a" opt; do
+	case $opt in
+		a)
+			echo "-a was triggered!" >&2
 			;;
-		p)
-			p=${OPTARG}
-			;;
-		*)
-			usage
+		\?)
+			echo "Invalid Option: -$OPTARG" >&2
 			;;
 	esac
 done
-shift $((OPTIND-1))
-if [ -z "${s}" ] || [ -z "${p}" ]; then
-	usage
-fi
-
-echo "s = ${s}"
-echo "p = ${p}"
-
 
