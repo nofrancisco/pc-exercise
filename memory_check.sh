d466c5454237153f97ad0248d09cf70b7a128239
Nico <nicoloofrancisco@gmail.com>
2016-12-29 22:02:36 +0800

    Edited console output

--- memory_check.sh
+++ memory_check.sh
@@ -1,21 +1,16 @@
 \#!/bin/bash
 clear
-Total_memory=$(free -m | grep Mem: | awk '{print $2}')
-#echo "Total System Memory: "$Total_memory "MB"
-Used_memory=$(free -m | grep Mem: | awk '{print $3}')
-#echo "Used Memory: "$Used_memory "MB"
-Free_memory=$(free -m | grep Mem: | awk '{print $4}')
-#echo "Free Memory: "$Free_memory "mb"
 
+Total_memory=$(free -m | grep Mem: | awk '{print $2}')
+Used_memory=$(free -m | grep Mem: | awk '{print $3}')
+Free_memory=$(free -m | grep Mem: | awk '{print $4}')
 
 while getopts ":c: :w: :e:" opt; do
 	case $opt in
 		c) 
 			critical_threshold=${OPTARG}
 			#echo "Critical Threshold (%) =" $critical_threshold
-			let "critical_threshold = ($Total_memory * $critical_threshold / 100)"
-			
-			
+			let "critical_threshold = ($Total_memory * $critical_threshold / 100)"		
 			;;
 			
 		w)
@@ -36,7 +31,10 @@ while getopts ":c: :w: :e:" opt; do
 			;;
 
 		:)
-			echo "Supply Required Arguments" >&2
+			echo "Supply the following arguments"
+			echo "-c Critical Threshold"
+			echo "-w Warning Threshold"
+			echo  "-e Email Address" >&2
 			exit 1
 			;;
 	esac
@@ -44,21 +42,23 @@ done
 
 if [ $critical_threshold -lt $warning_threshold ]
 	then
-		echo "Critical Threshold ($critical_threshold ) MB should be greater than Warning Threshold ($warning_threshold) MB"
+		echo "Critical Threshold ($critical_threshold) MB should be greater than Warning Threshold ($warning_threshold) MB"
 		exit 1
 fi
 
 
 if [ $Used_memory -ge $critical_threshold ]
 	then
+		dt=$(date '+%Y/%m/%d %H:%M');
 		
 		echo "Total System Memory: "$Total_memory "MB"
 		echo "Used Memory: "$Used_memory "MB"
 		echo "Critical Threshold: " $critical_threshold "MB"
 		echo "Warning Threshold: " $warning_threshold "MB"
 		echo "2. Used Memory ($Used_memory) is greater than or equal to critical threshold ($critical_threshold) mb."
+		echo "Date: $dt"
 		echo "Top 10 Processes: "
-		ps -eo pid,cmd,%mem,user,args --sort -rss | head -10
+		ps -eo pid,%mem,user,args --sort -rss | head -10
 		exit 1
 fi
 
